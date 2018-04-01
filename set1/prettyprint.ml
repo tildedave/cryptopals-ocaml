704fbcc0d98fff45a59ac76a3aa714a92b8a47d0
Dave King <tildedave@gmail.com>
2018-03-31 19:30:03 -0700

    Challenge 2

--- set1/prettyprint.ml
+++ set1/prettyprint.ml
@@ -1,14 +1,5 @@
 #use "util.ml"
 
-module type PrettyPrint_type = sig
-  val to_hex_string : bytes -> string
-  val from_hex_string : string -> bytes
-  val from_base64_string : string -> bytes
-  val to_base64_string : bytes -> string
-  val base64_char_to_int : char -> int
-  val hex_char_to_int : char -> int
-end
-
 module PrettyPrint : PrettyPrint_type = struct
   let hex_alphabet = "0123456789abcdef"
   let base64_alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
